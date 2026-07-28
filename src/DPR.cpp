0c3d4b6d27b405c8d01384fdf82bd8d7c33a4986
gdwnldsKSC <gary@kisaracorporation.com>
2026-07-28 13:20:04 -0400

    DPR: Fix missed reporting cases due to additional populated banks.

--- src/DPR.cpp
+++ src/DPR.cpp
@@ -584,6 +584,30 @@ void CDPR::WriteMem(int index, u64 address, int dsize, u64 data)
 			case 6:
 			case 7:
 			case 8:
+			case 0x09: // MMB1..MMB3 DIMM regions (see map above)
+			case 0x0a:
+			case 0x0b:
+			case 0x0c:
+			case 0x0d:
+			case 0x0e:
+			case 0x0f:
+			case 0x10:
+			case 0x11:
+			case 0x12:
+			case 0x13:
+			case 0x14:
+			case 0x15:
+			case 0x16:
+			case 0x17:
+			case 0x18:
+			case 0x19:
+			case 0x1a:
+			case 0x1b:
+			case 0x1c:
+			case 0x1d:
+			case 0x1e:
+			case 0x1f:
+			case 0x20:
 			case 0x25:
 			case 0x26:
 			case 0x27:
