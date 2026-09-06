f787e42be2bb72278bf072d9bf6db910671b3126
Martin Vorländer <martin.vorlaender@t-online.de>
2026-09-06 09:38:22 +0200

    Restrict curses windows to screen size. Abort if screen is too small to display a form.

--- src/es40-cfg-tui.cpp
+++ src/es40-cfg-tui.cpp
@@ -414,6 +414,11 @@ WINDOW *create_window(
     if (helptext != NULL)
         set_min_width(&nCols, helptext);
 
+    if (nLines > scrh)
+        nLines = scrh - 2;
+    if (nCols > scrw)
+        nCols = scrw - 2;
+
     if (begin_y == -1 && begin_x == -1)
     {
         begin_y = (scrh - nLines - 2) / 2;
@@ -622,7 +627,10 @@ FormValues_t show_form(
         set_min_width(&nCols, entry[i].preset);
     }
 
-    WINDOW *my_win = create_window(nLines + 1, nCols + 1, -1, -1, title, helptext);
+    if (nLines + 2 > scrh)
+        FAILURE_1(Runtime, "Screen is too small for form, need %d lines", nLines + 2);
+
+    WINDOW *my_win = create_window(nLines, nCols + 1, -1, -1, title, helptext);
     PANEL *my_panel = new_panel(my_win);
     update_panels();
 
@@ -1610,7 +1618,7 @@ void validation_ev68cb_max_ticks(FIELD *field)
 // Form for EV68CB
 void edit_ev68cb(const char *title)
 {
-    vector<FormEntry_t> entry; // (enabled, speed [,nohle]) * MAX_CPUS
+    vector<FormEntry_t> entry; // (enabled, speed, max_instr_per_tick [,nohle]) * MAX_CPUS
 
     for (int i = 0; i < MAX_CPUS; ++i)
     {
@@ -3024,12 +3032,19 @@ int main(int argc, char **argv)
     signal(SIGWINCH, resizeHandler);
 #endif
 
-    es40_banner("AlphaServer ES40 emulator configuration utility");
+    try
+    {
+        es40_banner("AlphaServer ES40 emulator configuration utility");
 
-    bool save_results = main_menu();
+        bool save_results = main_menu();
 
-    if (save_results)
-        write_configuration(out_filename);
+        if (save_results)
+            write_configuration(out_filename);
+    }
+	catch (CException& e)
+	{
+		printf("Failure: %s\n", e.displayText().c_str());
+    }
 
     return 0;
 }
